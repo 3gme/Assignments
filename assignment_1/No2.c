a8779b45c06e0236ab700c242452eed0800a9813
mahmoud ezzat <mahmoud.ezzat1234123121@gmail.com>
2024-02-04 17:57:07 +0200

    assignment_1 last

--- assignment_1/No2.c
+++ assignment_1/No2.c
@@ -7,7 +7,7 @@ Write a C code that will ask the user to enter a value then print it.
 int main(){
     printf("enter a value you want to print\n");
     float value = 0;
-    scanf("%d",&value);
-    printf("the value you entered is : %d",value);
+    scanf("%f",&value);
+    printf("the value you entered is : %.5f",value);
     return 0;
 }
